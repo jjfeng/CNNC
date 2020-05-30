b7d83a4a9052425a763e2d75376052228ed27c90
Jean Feng <jean.feng@gmail.com>
2020-05-30 09:43:46 -0700

    fit an easier net

--- train_with_labels_wholedatax_easiernet.py
+++ train_with_labels_wholedatax_easiernet.py
@@ -5,13 +5,13 @@ from __future__ import print_function
 #srun -p gpu --gres=gpu:1 -c 2 --mem=20Gb python train_with_labels_wholedatax.py 9 /home/yey3/cnn_project/code3/NEPDF_data 3 > results_whole.txt
 #######################OUTPUT
 # it will generate a folder 'wholeXXXXX', in which 'keras_cnn_trained_model_shallow.h5' is the final trained model
-import keras
-from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Conv2D, MaxPooling2D
-from keras.optimizers import SGD
-from keras.callbacks import EarlyStopping,ModelCheckpoint
+#import keras
+#from keras.preprocessing.image import ImageDataGenerator
+#from keras.models import Sequential
+#from keras.layers import Dense, Dropout, Activation, Flatten
+#from keras.layers import Conv2D, MaxPooling2D
+#from keras.optimizers import SGD
+#from keras.callbacks import EarlyStopping,ModelCheckpoint
 import os,sys
 import numpy as np
 import matplotlib
@@ -22,6 +22,8 @@ from scipy import interp
 
 ###############
 # Jean modifications
+import logging
+import torch
 
 from spinn2.sier_net import SierNetEstimator
 ####################################### parameter settings
@@ -29,10 +31,21 @@ data_augmentation = False
 # num_predictions = 20
 batch_size = 32 # mini batch for training
 #num_classes = 3   #### categories of labels
-epochs = 200      #### iterations of trainning, with GPU 1080, each epoch takes about 60s
+epochs = 2      #### iterations of trainning, with GPU 1080, each epoch takes about 60s
 #length_TF =3057  # number of divide data parts
 # num_predictions = 20
 model_name = 'jean_test_model.h5'
+
+log_file = "_output/jean_log.txt"
+
+seed = 0
+n_layers = 5
+n_hidden = 100
+full_tree_pen = 0.1
+input_pen = 0.1
+num_batches = 3
+max_iters = 500
+max_prox_iters = 200
 ###################################################
 
 
@@ -61,94 +74,81 @@ def load_data_TF2(indel_list,data_path): # cell type specific  ## random samples
 if len(sys.argv) < 4:
     print ('No enough input files')
     sys.exit()
+
+logging.basicConfig(
+    format="%(message)s", filename=log_file, level=logging.DEBUG
+)
+
 length_TF =int(sys.argv[1]) # number of data parts divided
 data_path = sys.argv[2]
 num_classes = int(sys.argv[3])
 whole_data_TF = [i for i in range(length_TF)]
 (x_train, y_train,count_set_train) = load_data_TF2(whole_data_TF,data_path)
+x_train = x_train.reshape((x_train.shape[0],-1))
+n_obs = x_train.shape[0]
+n_inputs = x_train.shape[1]
 print(x_train.shape, 'x_train samples')
 save_dir = os.path.join(os.getcwd(), '_output', 'whole_model_test')
-if num_classes > 2:
-    y_train = keras.utils.to_categorical(y_train, num_classes)
+print(y_train)
+#if num_classes > 2:
+#    y_train = keras.utils.to_categorical(y_train, num_classes)
+y_train = y_train.reshape((y_train.size, 1))
 print(y_train.shape, 'y_train samples')
     ###########
 if not os.path.isdir(save_dir):
     os.makedirs(save_dir)
     ############
-model = Sequential()
-model.add(Conv2D(32, (3, 3), padding='same',input_shape=x_train.shape[1:]))
-model.add(Activation('relu'))
-#model.add(Conv2D(32, (3, 3)))
-#model.add(Activation('relu'))
-#model.add(MaxPooling2D(pool_size=(2, 2)))
-#model.add(Dropout(0.5))
-#
-#model.add(Conv2D(64, (3, 3), padding='same'))
-#model.add(Activation('relu'))
-#model.add(Conv2D(64, (3, 3)))
-#model.add(Activation('relu'))
-#model.add(MaxPooling2D(pool_size=(2, 2)))
-#model.add(Dropout(0.5))
-#
-#model.add(Conv2D(128, (3, 3), padding='same'))
-#model.add(Activation('relu'))
-#model.add(Conv2D(128, (3, 3)))
-#model.add(Activation('relu'))
-#model.add(MaxPooling2D(pool_size=(2, 2)))
-#model.add(Dropout(0.5))
-#
-#model.add(Flatten())
-#model.add(Dense(512))
-#model.add(Activation('relu'))
-#model.add(Dropout(0.5))
-model.add(Flatten())
-model.add(Dense(100))
-model.add(Dropout(0.15))
-model.add(Dense(100))
-model.add(Dropout(0.15))
-if num_classes < 2:
-    print('no enough categories')
-    sys.exit()
-elif num_classes == 2:
-    model.add(Dense(1, activation='sigmoid'))
-    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-    model.compile(optimizer=sgd, loss='binary_crossentropy', metrics=['accuracy'])
-else:
-    model.add(Dense(num_classes))
-    model.add(Activation('softmax'))
-    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-    model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
+estimator = SierNetEstimator(
+    n_inputs=n_inputs,
+    input_filter_layer=True,
+    n_layers=n_layers,
+    n_hidden=n_hidden,
+    n_out=num_classes,
+    full_tree_pen=full_tree_pen,
+    input_pen=input_pen,
+    batch_size=(n_obs // num_batches + 1),
+    num_classes=num_classes,
+    # Weight classes by inverse of their observed ratios. Trying to balance classes
+    weight=(n_obs / (num_classes * np.bincount(y_train.flatten()))
+    if num_classes >= 2
+    else None),
+)
+torch.manual_seed(seed)
+estimator.fit(
+    x_train, y_train, max_iters=max_iters, max_prox_iters=max_prox_iters
+)
+print("SUCCESS")
 
-early_stopping = keras.callbacks.EarlyStopping(monitor='val_accuracy', patience=300, verbose=0, mode='auto')
-checkpoint1 = ModelCheckpoint(filepath=save_dir + '/weights.{epoch:02d}-{val_loss:.2f}.hdf5', monitor='val_loss',verbose=1, save_best_only=False, save_weights_only=False, mode='auto', period=1)
-checkpoint2 = ModelCheckpoint(filepath=save_dir + '/weights.hdf5', monitor='val_accuracy', verbose=1,save_best_only=True, mode='auto', period=1)
-callbacks_list = [checkpoint2, early_stopping]
-if not data_augmentation:
-    print('Not using data augmentation.')
-    history = model.fit(x_train, y_train,batch_size=batch_size,epochs=epochs,validation_split=0.2,shuffle=True, callbacks=callbacks_list)
-
-    # Save model and weights
-
-model_path = os.path.join(save_dir, model_name)
-model.save(model_path)
-print('Saved trained model at %s ' % model_path)
-    # Score trained model.
-############################################################################## plot training process
-plt.figure(figsize=(10, 6))
-plt.subplot(1,2,1)
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.grid()
-plt.legend(['train', 'val'], loc='upper left')
-plt.subplot(1,2,2)
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'val'], loc='upper left')
-plt.grid()
-plt.savefig(save_dir+'/end_result.pdf')
+#early_stopping = keras.callbacks.EarlyStopping(monitor='val_accuracy', patience=300, verbose=0, mode='auto')
+#checkpoint1 = ModelCheckpoint(filepath=save_dir + '/weights.{epoch:02d}-{val_loss:.2f}.hdf5', monitor='val_loss',verbose=1, save_best_only=False, save_weights_only=False, mode='auto', period=1)
+#checkpoint2 = ModelCheckpoint(filepath=save_dir + '/weights.hdf5', monitor='val_accuracy', verbose=1,save_best_only=True, mode='auto', period=1)
+#callbacks_list = [checkpoint2, early_stopping]
+#if not data_augmentation:
+#    print('Not using data augmentation.')
+#    history = model.fit(x_train, y_train,batch_size=batch_size,epochs=epochs,validation_split=0.2,shuffle=True, callbacks=callbacks_list)
+#
+#    # Save model and weights
+#
+#model_path = os.path.join(save_dir, model_name)
+#model.save(model_path)
+#print('Saved trained model at %s ' % model_path)
+#    # Score trained model.
+############################################################################### plot training process
+#plt.figure(figsize=(10, 6))
+#plt.subplot(1,2,1)
+#plt.plot(history.history['accuracy'])
+#plt.plot(history.history['val_accuracy'])
+#plt.title('model accuracy')
+#plt.ylabel('accuracy')
+#plt.xlabel('epoch')
+#plt.grid()
+#plt.legend(['train', 'val'], loc='upper left')
+#plt.subplot(1,2,2)
+#plt.plot(history.history['loss'])
+#plt.plot(history.history['val_loss'])
+#plt.title('model loss')
+#plt.ylabel('loss')
+#plt.xlabel('epoch')
+#plt.legend(['train', 'val'], loc='upper left')
+#plt.grid()
+#plt.savefig(save_dir+'/end_result.pdf')
